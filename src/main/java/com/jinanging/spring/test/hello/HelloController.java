a29614647fc762a0830ca5e7243e9ab761e63af7
Jinan <wkdejrrnjs@naver.com>
2025-05-16 18:03:26 +0900

    [권진환] 최초 test 깃 연동

---
+++ src/main/java/com/jinanging/spring/test/hello/HelloController.java
@@ -0,0 +1,19 @@
+package com.jinanging.spring.test.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String Hello() {
+		return "Hello Wolrd!";
+	}
+	
+	
+	
+
+}
